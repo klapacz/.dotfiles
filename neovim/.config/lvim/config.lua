c230504539f30e3b55dcc64bfef727fdfe3697dc
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-10-15 00:06:14 +0200

    chore: new keymaps

--- neovim/.config/lvim/config.lua
+++ neovim/.config/lvim/config.lua
@@ -12,3 +12,15 @@ lvim.keys.normal_mode["gT"] = "<cmd>terminal<cr>"
 lvim.keys.normal_mode["gN"] = "<cmd>tabnew<cr>"
 lvim.keys.normal_mode["H"] = "<cmd>tabprev<cr>"
 lvim.keys.normal_mode["L"] = "<cmd>tabnext<cr>"
+
+lvim.keys.normal_mode["<leader>p"] = "\"_dP"
+lvim.keys.visual_mode["<leader>p"] = "\"_dP"
+
+lvim.keys.normal_mode["<leader>d"] = "\"_d"
+lvim.keys.visual_mode["<leader>d"] = "\"_d"
+
+lvim.keys.normal_mode["<leader>y"] = "\"+y"
+lvim.keys.visual_mode["<leader>y"] = "\"+y"
+
+-- Do not use system clipboard
+vim.opt.clipboard = ""
--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,10 +1,17 @@
 local iscode = function()
-    return vim.fn.exists('g:vscode') == 1
+  return vim.fn.exists('g:vscode') == 1
 end
+
+-- Set leader key to space
+vim.g.mapleader = ' '
 
 
 if iscode() then
-    vim.keymap.set("n", "gN", "<cmd>Tabnew<cr>")
-    vim.keymap.set("n", "H", "<cmd>Tabprev<cr>")
-    vim.keymap.set("n", "L", "<cmd>Tabnext<cr>")
+  vim.keymap.set("n", "gN", "<cmd>Tabnew<cr>")
+  vim.keymap.set("n", "H", "<cmd>Tabprev<cr>")
+  vim.keymap.set("n", "L", "<cmd>Tabnext<cr>")
 end
+
+vim.keymap.set({ "n", "v" }, "<leader>p", "\"_dP")
+vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
+vim.keymap.set({ "n", "v" }, "<leader>y", "\"+y")
