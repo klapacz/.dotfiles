a4824aca8f37c8bca5b91842135267c7b0f58cb6
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-10-15 02:02:02 +0200

    feat: vscode settings

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,17 +1,12 @@
-local iscode = function()
-  return vim.fn.exists('g:vscode') == 1
-end
-
 -- Set leader key to space
 vim.g.mapleader = ' '
-
-
-if iscode() then
-  vim.keymap.set("n", "gN", "<cmd>Tabnew<cr>")
-  vim.keymap.set("n", "H", "<cmd>Tabprev<cr>")
-  vim.keymap.set("n", "L", "<cmd>Tabnext<cr>")
-end
 
 vim.keymap.set({ "n", "v" }, "<leader>p", "\"_dP")
 vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
 vim.keymap.set({ "n", "v" }, "<leader>y", "\"+y")
+
+if vim.g.vscode then
+  vim.keymap.set("n", "gN", "<cmd>Tabnew<cr>")
+  vim.keymap.set("n", "H", "<cmd>Tabprev<cr>")
+  vim.keymap.set("n", "L", "<cmd>Tabnext<cr>")
+end
