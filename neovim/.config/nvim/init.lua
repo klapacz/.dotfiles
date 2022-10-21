fd52f905de5d5931988428eba7b89b2c3d928e85
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-10-21 23:18:41 +0200

    chore: tweaks

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -6,6 +6,8 @@ vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
 vim.keymap.set({ "n", "v" }, "<leader>y", "\"+y")
 
 if vim.g.vscode then
+  vim.keymap.set("n", "<leader>w", "<cmd>Write<cr>")
+
   vim.keymap.set("n", "gN", "<cmd>Tabnew<cr>")
   vim.keymap.set("n", "H", "<cmd>Tabprev<cr>")
   vim.keymap.set("n", "L", "<cmd>Tabnext<cr>")
