aa5baad28858d2eac02ea61f6801de0541756a39
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-11-26 13:36:47 +0100

    chore

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,11 +3,17 @@ if test -e $brew_bin
 	eval ($brew_bin shellenv)
 end
 
-fish_vi_key_bindings
-
 starship init fish | source
 
 alias rm trash
 alias serve 'python3 -m http.server'
 alias ymd "date '+%Y-%m-%d'"
+
+set fish_greeting
+set -gx LANG 'en_US.UTF-8'
+set -gx EDITOR 'nvim'
+
+
+fish_vi_key_bindings
+bind -M insert \co 'commandline l; commandline -f execute'
 
