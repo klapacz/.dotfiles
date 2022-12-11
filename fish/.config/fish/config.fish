2f84cd2b2ff761a6dd62171f545f1c484fe6b260
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-12-11 01:50:37 +0100

    chore: add abbr's

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,3 +12,6 @@ set -gx EDITOR 'nvim'
 fish_vi_key_bindings
 bind -M insert \co 'commandline n; commandline -f execute'
 
+
+abbr --add gpg-decrypt gpg --encrypt --recipient lapacz.kornel@gmail.com file
+abbr --add gpg-encrypt gpg --decrypt file.gpg > file
