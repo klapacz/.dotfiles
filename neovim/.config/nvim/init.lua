1b8312e6cc2e06ce5340fa498ebc3a1e0257f2cf
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2023-03-14 23:59:05 +0100

    chore: add toggleterm and luapad plugins

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -34,6 +34,26 @@ require('lazy').setup({
 
   "jamessan/vim-gnupg",
   "nickeb96/fish.vim",
+  "rafcamlet/nvim-luapad",
+
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = function ()
+      require("toggleterm").setup()
+
+      vim.cmd([[
+        " set
+        autocmd TermEnter term://*toggleterm#*
+              \ tnoremap <silent><c-t> <Cmd>exe v:count1 . "ToggleTerm"<CR>
+        " By applying the mappings this way you can pass a count to your
+        " mapping to open a specific window.
+        " For example: 2<C-t> will open terminal 2
+        nnoremap <silent><c-t> <Cmd>exe v:count1 . "ToggleTerm"<CR>
+        inoremap <silent><c-t> <Esc><Cmd>exe v:count1 . "ToggleTerm"<CR>
+      ]])
+    end
+  },
 
   {
     'nvim-tree/nvim-tree.lua',
